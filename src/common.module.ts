6cb8b3fdc1bb0fe6063d3a254f5974189c662545
Prashant Sachan <psachan190@gmail.com>
2022-07-15 09:50:43 +0530

    fix: changed entities to folder

--- src/common.module.ts
+++ src/common.module.ts
@@ -1,24 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CommonController } from './common.controller';
 import { CommonService } from './common.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
 import { validate } from './env.validation';
-import { User } from './entities/user.entity';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ 
 			envFilePath: `${process.env.NODE_ENV}.env`,
 			validate,
-		}), 
-		TypeOrmModule.forRoot({
-			type: 'mysql',
-			... configuration().database,
-			entities: [User],
-			synchronize: true,
-	})],
+		})],
 	controllers: [CommonController],
 	providers: [CommonService],
 })
